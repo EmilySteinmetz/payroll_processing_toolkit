e31b3e58adbf17a7ce96e1790e7e0602cb734ac0
Emily Steinmetz <emilysteinmetz@usf.edu>
2026-06-05 19:15:40 -0400

    Add employee data to code_cc_5.js

---
+++ code_cc_5.js
@@ -0,0 +1,6 @@
+const employees = [
+    { name: "Ariana Grande", hourlyRate: 63, hoursWorked: 45 },
+    { name: "Tate McRae", hourlyRate: 35, hoursWorked: 25 },
+    { name: "Olivia Rodrigo", hourlyRate: 40, hoursWorked: 15 },
+    { name: "Sabrina Carpenter", hourlyRate: 60, hoursWorked: 35 }
+];
